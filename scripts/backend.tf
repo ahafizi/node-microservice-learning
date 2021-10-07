225afca32947e22eec7527d690e9f702d1040e6a
ahafizi <a.hafizov@team.alif.tj>
2021-10-07 15:49:05 +0500

    revverse backend.tf

--- scripts/backend.tf
+++ scripts/backend.tf
@@ -3,9 +3,9 @@
 
 terraform {
   backend "azurerm" {
-    resource_group_name = "tajtube"
-    storage_account_name = "tajtube"
-    container_name = "tajtube"
+    resource_group_name = "tajtube-terraform"
+    storage_account_name = "tajtubeterraform"
+    container_name = "terraform-state"
     key = "terraform.tfstate"
   }
 }
